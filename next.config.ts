81954466fa26acfc6a38e50419a24c29854499e4
maxwell cofie <maxcofie@gmail.com>
2025-10-21 12:23:05 +0000

    Push first version

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const SUPABASE_HOST = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname;
+// const SUPABASE_HOST = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname;
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+if (!SUPABASE_URL) {
+    throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL environment variable");
+}
+const SUPABASE_HOST = new URL(SUPABASE_URL).hostname;
 
 const withPWA = require('next-pwa')({
     dest: 'public',
